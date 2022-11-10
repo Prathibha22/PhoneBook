f620052a4110c39f0839b142100da52335b8314b
Prathibha <prathibha@Zipcoders-MacBook-Pro-8.local>
2022-11-09 22:47:55 -0500

    Implemented all methods in phonebook

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,11 +1,8 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 //import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+
 
 /**
  * Created by leon on 1/23/18.
@@ -16,39 +13,44 @@ public class PhoneBook {
     private final Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+        this.phonebook = map;
     }
 
-    public PhoneBook() {
-        this(null);
-    }
+    public PhoneBook() { this.phonebook=new HashMap<>();}
 
     public void add(String name, String phoneNumber) {
+      phonebook.put(name, Collections.singletonList(phoneNumber));
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        phonebook.put(name,Arrays.asList(phoneNumbers));
     }
 
-    public void remove(String name) {
-    }
+    public void remove(String name) {phonebook.remove(name); }
 
     public Boolean hasEntry(String name) {
-        return null;
+        return phonebook.containsKey(name);
     }
 
-    public List<String> lookup(String name) {
-        return null;
-    }
+    public List<String> lookup(String name) { return phonebook.get(name);}
 
     public String reverseLookup(String phoneNumber)  {
+        for(Map.Entry<String,List<String>> entry:phonebook.entrySet()){
+            if((entry.getValue().toString().replaceAll("[\\[\\],]","")).equals(phoneNumber)){
+                return entry.getKey();
+            }
+        }
         return null;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        Set<String> setNames=phonebook.keySet();
+        List<String> names=new ArrayList<>(setNames);
+        Collections.sort(names,Collections.reverseOrder());
+        return names;
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return this.phonebook;
     }
 }
